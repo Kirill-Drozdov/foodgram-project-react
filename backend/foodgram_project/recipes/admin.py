7d3f75e4c5f56265bf9d46963f45d70edaa4313e
Kirill Drozdov <kirill.drozdov1998@yandex.ru>
2022-10-09 08:49:14 +0500

    Create Ingredient model

--- backend/foodgram_project/recipes/admin.py
+++ backend/foodgram_project/recipes/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from recipes.models import Tag
+from recipes.models import Tag, Ingredients
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'color', 'slug')
+    list_editable = ('color', 'slug')
+
+
+@admin.register(Ingredients)
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount', 'measurement_unit')
+    list_editable = ('amount', 'measurement_unit')
--- backend/foodgram_project/recipes/models.py
+++ backend/foodgram_project/recipes/models.py
@@ -7,11 +7,13 @@ User = get_user_model()
 class Tag(models.Model):
     name = models.CharField(
         'Название',
-        max_length=100
+        max_length=100,
+        unique=True
     )
     color = models.CharField(
         'Цветовой HEX-код',
-        max_length=16
+        max_length=16,
+        unique=True
     )
     slug = models.SlugField(
         'Slug',
@@ -25,8 +27,27 @@ class Tag(models.Model):
     def __str__(self) -> str:
         return self.name
 
-# class Ingredients(models.Model):
-#     pass
+
+class Ingredients(models.Model):
+    name = models.CharField(
+        'Название ингредиента',
+        max_length=100,
+        unique=True
+    )
+    amount = models.IntegerField(
+        'Количество'
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения',
+        max_length=15,
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
 
 
 # class Recipe(models.Model):
